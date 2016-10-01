eb5c84357b7010ebbe8a8214a835f2bd40c7b387
lewisgit <lewisreg@126.com>
2016-10-01 14:39:55 +0800

    boot loader

---
+++ bootasm.s
@@ -0,0 +1,62 @@
+.code16
+.globl start
+start:
+	cli
+	xorw %ax，%ax
+	movw %ax, %ds
+	movw %ax, %es
+	movw %ax, %ss
+
+seta20_1:
+	inb $0x64, %al
+	testb $0x2, %al
+	jnz seta20_1
+
+	movb $0xd1,%al
+	outb %al,$0x64
+seta20_2:
+	inb $0x64,%al
+	testb $0x2,%al
+	jnz seta20_2
+
+	movb $0xdf,%al
+	outb %al,$0x60
+
+
+lgdt gdtptr
+
+movl %cr0,%eax
+orl $0x2,%eax
+movl %eax,%cr0
+
+ljmp $0x08,start32
+
+start32:
+	movw $0x10,%eax
+	movw %eax,%ds
+	movw %eax,%ss
+	movw %eax,%es
+	movw $0,%eax
+	movw %eax,%gs
+	movew %eax,fs
+
+	movl $start,%esp
+	call bootmain
+
+spin: jmp spin
+
+.p2align 2
+gdt: 
+	.word 0,0,0,0
+	.word 0xffff
+	.word 0
+	.word 0x9a00
+	.word 0x00cf
+	.word 0xffff
+	.word 0
+	.word 0x9200
+	.word 0x00cf
+gdtptr:
+	.word (gdtptr-gdt-1)
+	.long gdt
+
